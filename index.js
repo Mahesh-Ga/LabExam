c933659a8423364e5918db228aaa88cebb259cff
Mahesh-Ga <68686037+Mahesh-Ga@users.noreply.github.com>
2023-07-10 22:36:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(cors('*'))
 app.use(express.json())
 
 app.get('/', (request, response) => {
-  response.send('Home page')
+  response.send('My Home page')
 })
 
 const empRouter = require('./Routes/emp')
